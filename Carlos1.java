d7dd0923c084be0877e883e283047407008cf4b8
Daniel-22-16 <1475937@senati.pe>
2023-03-17 10:51:47 -0500

    java5

--- Carlos1.java
+++ Carlos1.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class Carlos1 {
 
     public static void main(String[] args) {
-    }    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre =sc.nextLine();
+    }
+        
     
 }
